54c957b479607843bae3658bc8ac5511781aed89
Sonia Huoy <vsoniahuoy@gmail.com>
2019-07-26 02:52:18 +0000

    CarrierWave S3

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'popper_js', '~> 1.11.1'
 gem 'bootstrap', '4.0.0.alpha6'
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave', '>=2.0.0.rc', '< 3.0'
+gem "figaro"
+gem "fog-aws"
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,6 +2,7 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
+    @grams = Gram.all
   end
 
 
@@ -67,7 +68,7 @@ class GramsController < ApplicationController
 
   def gram_params
     # pull the data
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
 
